b106f5ebf044e113f051c6248b86f887735373f3
Laxmi Pattar <laxmipattar710@gmail.com>
2019-07-18 17:43:46 +0530

    perfect_numbers

--- test/test_perfect_num1.rb
+++ test/test_perfect_num1.rb
@@ -5,5 +5,7 @@ class TestInteger < Minitest::Test
 	def test_perfect
 		assert_equal false, 5.is_perfect_num?
   	assert_equal true, 28.is_perfect_num?
+  	assert_equal false, 2.is_perfect_num?
+  	assert_equal false, 8.is_perfect_num?
 	end
 end
