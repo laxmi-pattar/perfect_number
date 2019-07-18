8a5b0f0d6c1a5d0abcc87ddb8607b767af8f63eb
Laxmi Pattar <laxmipattar710@gmail.com>
2019-07-18 17:04:17 +0530

    perfect_number

---
+++ test/test_perfect_num1.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require_relative '../perfect_num1'
+
+class TestInteger < Minitest::Test
+	def test_perfect
+		assert_equal false, 5.is_perfect_num?
+  	assert_equal true, 28.is_perfect_num?
+	end
+end
